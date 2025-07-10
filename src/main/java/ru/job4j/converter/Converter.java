ffc8c64cdfe45a9b21a80b7c166b0c6060d758ab
bamvam <kyrartikal@gmail.com>
2025-07-10 19:35:11 +0300

    1. Модульные тесты [#505277 #547703]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,12 +2,12 @@ package ru.job4j.converter;
 
 public class Converter {
     public static double rubleToEuro(double value) {
-        // Assuming the conversion rate is 1 Euro = 90 Rubles
+        //  Assuming the conversion rate is 1 Euro = 90 Rubles
         return value / 90;
     }
 
     public static double rubleToDollar(double value) {
-        // Assuming the conversion rate is 1 Dollar = 75 Rubles
+        //  Assuming the conversion rate is 1 Dollar = 75 Rubles
         return value / 75;
     }
 }
