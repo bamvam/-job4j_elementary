a97363364dd8bf98ad5e3e82c1f1032db350b90a
bamvam <kyrartikal@gmail.com>
2025-07-15 14:39:47 +0300

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Artur Morgan";
+        System.out.println(names[0]);
+        names[1] = "Gorister Honter";
+        System.out.println(names[1]);
+        names[2] = "Barack Obama";
+        System.out.println(names[2]);
+        names[3] = "Harlan Elison";
+        System.out.println(names[3]);
     }
 }
