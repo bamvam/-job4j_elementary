8d9a491ec49848cc93720d4dd530e05b64278a83
bamvam <kyrartikal@gmail.com>
2025-07-15 10:32:07 +0300

    5.6. Простые числа [#505215 #548314]

--- src/main/java/ru/job4j/loop/PrimeNumber.java
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -4,22 +4,10 @@ public class PrimeNumber {
     public static int calc(int finish) {
         int count = 0;
         for (int i = 2; i <= finish; i++) {
-            if (isPrime(i)) {
+            if (CheckPrimeNumber.check(i)) {
                 count++;
             }
         }
         return count;
-    }
-
-    private static boolean isPrime(int number) {
-        if (number < 2) {
-            return false;
-        }
-        for (int i = 2; i * i <= number; i++) {
-            if (number % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
