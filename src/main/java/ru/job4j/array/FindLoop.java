f44d509d73a14307987a1f1f5cf93e95950406e9
bamvam <kyrartikal@gmail.com>
2025-07-24 19:03:59 +0300

    Поиск индекса в диапазоне [#505237]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,13 @@ public class FindLoop {
         }
         return result;
     }
+
+    public static int indexInRange(int[] data, int element, int start, int finish) {
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == element) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
