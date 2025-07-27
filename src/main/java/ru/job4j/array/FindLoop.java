b176212dd8d8ed836599db036dbfdccdb91c7403
bamvam <kyrartikal@gmail.com>
2025-07-27 12:06:59 +0300

    Поиск индекса в диапазоне [#505237 #549119]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 public class FindLoop {
+
     public static int indexOf(int[] data, int element) {
         int result = -1; /* если элемента нет в массиве, то возвращаем -1. */
         for (int index = 0; index < data.length; index++) {
--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -24,7 +24,6 @@ class FindLoopTest {
         assertThat(result).isEqualTo(expected);
     }
 
-
     @Test
     public void whenDiapasonHas8ThenResultEqualFinish() {
         int[] data = new int[] {5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
